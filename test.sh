e532c01b842a82ce0b70feac463d9f0b622fa052
Marek Hanzal <marek.hanzal@x32.cz>
2018-10-10 18:42:31 +0200

    Common: ...

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-npm install
-npm run pretest
+npm install >/dev/null 2>&1
+npm run pretest >/dev/null 2>&1
 node_modules/.bin/nyc --reporter=text-summary node_modules/.bin/ava
