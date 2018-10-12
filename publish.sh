ee566862e1645fa5a67ebece7d7f686e73bea92f
Marek Hanzal <marek.hanzal@x32.cz>
2018-10-12 13:46:00 +0200

    CI: ...

--- publish.sh
+++ publish.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Resetting repo to the original state"
 git reset --hard
+echo "Bumping version"
 npm --no-git-tag-version version from-git
+echo "Let's publish the thing!"
 npm --no-git-tag-version publish --access public
