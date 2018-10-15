b433f6b080ea253cabf2b842943b351a488057d2
Marek Hanzal <marek.hanzal@x32.cz>
2018-10-15 15:07:34 +0200

    Common: Fixed interface implementation

--- src/view/view.ts
+++ src/view/view.ts
@@ -1,4 +1,8 @@
 import {IView} from "./types";
+import {HashMap} from "../collection";
 
 export abstract class AbstractView implements IView {
+	public abstract mount(attrs: HashMap<any>): IView;
+
+	public abstract umount(): IView;
 }
