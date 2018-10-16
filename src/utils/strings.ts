e81ba3a451853748043a0aa38962ba1eddb448bf
Marek Hanzal <marek.hanzal@x32.cz>
2018-10-16 14:06:31 +0200

    Common: Added kebab case string util

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -7,4 +7,8 @@ export class Strings {
 	public static unspace(string: string): string {
 		return (string.match(/[^\x20\t\r\n\f]+/g) || []).join(' ');
 	}
+
+	public static fromKebabCase(string: string): string {
+		return string.replace(/_\w/g, _ => _[1].toUpperCase());
+	}
 }
