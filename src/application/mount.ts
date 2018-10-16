8be4b0768eaa353b4410a09e94ddbd99440722b9
Marek Hanzal <marek.hanzal@x32.cz>
2018-10-16 17:16:05 +0200

    Common: Relaxed selectors

--- src/application/mount.ts
+++ src/application/mount.ts
@@ -18,7 +18,7 @@ export class Mount {
 	 * @param from
 	 * @param selector
 	 */
-	public views(from: Html, selector = 'template[data-view]'): Mount {
+	public views(from: Html, selector = '*[data-view]'): Mount {
 		from.selectorCollection(selector).each(html => this.viewManager.create(html.rattr('data-view')).bind(html));
 		return this;
 	}
@@ -29,7 +29,7 @@ export class Mount {
 	 * @param from
 	 * @param selector
 	 */
-	public controls(from: Html, selector: string = '.control[data-control]'): Mount {
+	public controls(from: Html, selector: string = '*[data-control]'): Mount {
 		from.selectorCollection(selector).each(html => this.container.create<IControl>(html.attr('data-control')).bind(html).mount());
 		return this;
 	}
