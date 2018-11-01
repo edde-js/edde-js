90d71f12b079dd5287961dd78c8acb2787cdbd72
Marek Hanzal <marek.hanzal@x32.cz>
2018-11-01 15:22:15 +0100

    View: Updated abstract view a bit

--- src/view/view.ts
+++ src/view/view.ts
@@ -1,9 +1,13 @@
 import {IView} from "./types";
 
 export abstract class AbstractView implements IView {
+	public mount(): IView {
+		return this;
+	}
+
+	public umount(): IView {
+		return this;
+	}
+
 	public abstract canHandle(path: string): boolean;
-
-	public abstract mount(): IView;
-
-	public abstract umount(): IView;
 }
