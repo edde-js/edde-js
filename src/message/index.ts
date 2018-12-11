8f5ba3f2a493b05dba3061f009753bf9f5a2df9b
Marek Hanzal <marek.hanzal@x32.cz>
2018-12-11 12:29:05 +0100

    Common: Oops, forgot to export message handler

--- src/message/index.ts
+++ src/message/index.ts
@@ -1,5 +1,6 @@
 export * from "./types";
 export * from "./message";
 export * from "./packet";
+export * from "./message-handler";
 export * from "./message-bus";
 export * from "./message-service";
