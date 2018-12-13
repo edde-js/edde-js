965056aca849bf5e19000f059b6e6d4d353b5e55
Marek Hanzal <marek.hanzal@x32.cz>
2018-12-13 14:54:34 +0100

    Common: Views are almost fully tested!

--- src/view/view-manager.spec.ts
+++ src/view/view-manager.spec.ts
@@ -2,7 +2,7 @@ import test from "ava";
 import {ContainerFactory, Make} from "../container";
 import {ViewManager} from "./view-manager";
 import {EventBus} from "../event";
-import {DeadRouteEvent, MountViewEvent, RefreshViewEvent} from "./events";
+import {DeadRouteEvent, MountViewEvent, RefreshViewEvent, UmountViewEvent} from "./events";
 import {AbstractView} from "./view";
 import {GetString, ToString} from "../utils";
 import {IView} from "./types";
@@ -18,22 +18,32 @@ class SomeView extends AbstractView {
 	}
 }
 
+@ToString('bar')
+class SomeSomeView extends AbstractView {
+	public canHandle(path: string): boolean {
+		return path === '/some-some';
+	}
+}
+
 test('ViewManager: Common Events', test => {
 	const container = ContainerFactory.container();
 	const viewManager = container.create<ViewManager>(ViewManager);
 	const eventBus = container.create<EventBus>(EventBus);
 	const templateManager = container.create<TemplateManager>(TemplateManager);
 	container.register(Runtime, function () {
-		return this.instance || (this.instance = new Runtime(new JSDOM('<body><div class="prdel is-hidden" data-template="boo"></div><main></main></body>').window));
+		return this.instance || (this.instance = new Runtime(new JSDOM('<body><div class="bardel is-hidden" data-template="bar"></div><div class="prdel is-hidden" data-template="boo"></div><main></main></body>').window));
 	});
 	const runtime = container.create<Runtime>(Runtime);
 	templateManager.bind(runtime.html());
 	container.register(SomeView, Make.service(SomeView));
+	container.register(SomeSomeView, Make.service(SomeSomeView));
 	viewManager.register(SomeView);
+	viewManager.register(SomeSomeView);
 	let deadEvent = null;
 	let routedView = null;
 	let routedPath = null;
 	let refresh = null;
+	let umount = null;
 	eventBus.listener(DeadRouteEvent).add((event: DeadRouteEvent) => {
 		deadEvent = event.getPath();
 	});
@@ -43,6 +53,9 @@ test('ViewManager: Common Events', test => {
 	});
 	eventBus.listener(RefreshViewEvent).add((event: RefreshViewEvent) => {
 		refresh = event.getView();
+	});
+	eventBus.listener(UmountViewEvent).add((event: UmountViewEvent) => {
+		umount = event.getView();
 	});
 	viewManager.routeTo('nope');
 	test.is(deadEvent, 'nope');
@@ -58,6 +71,8 @@ test('ViewManager: Common Events', test => {
 	test.is(runtime.require('main').getElement(), runtime.require('main').getElement());
 	test.is((<Html>(<any>view).root).getElement().outerHTML, '<div class="prdel"></div>');
 	test.is(runtime.require('body').getElement().outerHTML, '<body><main><div class="prdel"></div></main></body>');
+	test.truthy(viewManager.routeTo('/some-some'));
+	test.is(umount, view);
 });
 test('View: Types', test => {
 	test.false(IView.canHandle('/'));
